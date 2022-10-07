eef4d415024ea8e86ffc6655553e3c440c09cf8a
Alberto Uehara <alberto.uehara@gmail.com>
2022-10-07 10:54:33 -0300

    First Commit

---
+++ src/test/java/com/camel/camelmicroservice/CamelMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camel.camelmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
